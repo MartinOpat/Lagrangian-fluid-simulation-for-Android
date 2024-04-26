24e0a5824cda4520d3929b63f1893d7dec80c369
MartinOpat <martinopat.opat@gmail.com>
2024-04-26 11:34:48 +0200

    Refactored shaders

--- simulation/app/src/main/assets/fragment_shader.glsl
+++ simulation/app/src/main/assets/fragment_shader.glsl
@@ -10,8 +10,3 @@ void main() {
     }
 }
 
-//precision mediump float;
-//uniform vec4 u_Color;
-//void main() {
-//    gl_FragColor = u_Color;
-//}
--- simulation/app/src/main/assets/vertex_shader.glsl
+++ simulation/app/src/main/assets/vertex_shader.glsl
@@ -3,24 +3,11 @@ uniform bool uIsPoint; // Uniform to tell if we're rendering a point or triangle
 uniform vec3 uPosition;
 
 void main() {
-//    gl_Position = vPosition;
-//    vec3 pos = vPosition.xyz;
-
     if (uIsPoint) {
-        gl_PointSize = 100.0;
-//        pos += uVelocity * uTime;
-//
-//        // Wrap x coordinate
-//        if (pos.x > 1.0) pos.x = -1.0;
-//        else if (pos.x < -1.0) pos.x = 1.0;
-//
-//        // Wrap y coordinate
-//        if (pos.y > 1.0) pos.y = 1.0;
-//        else if (pos.y < -1.0) pos.y = -1.0;
+        gl_PointSize = 10.0;
         gl_Position = vec4(uPosition, 1.0);
 
     } else {
         gl_Position = vPosition;
     }
-
 }
