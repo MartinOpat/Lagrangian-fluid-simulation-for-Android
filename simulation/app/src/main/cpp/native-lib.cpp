e4e33531875a60656630bb0f2b50aac1e1d91f03
MartinOpat <martinopat.opat@gmail.com>
2024-04-28 09:42:41 +0200

    Added FPS counter

--- simulation/app/src/main/cpp/native-lib.cpp
+++ simulation/app/src/main/cpp/native-lib.cpp
@@ -28,6 +28,9 @@ float b = 0.8f;  // Drag coefficient
 std::vector<Particle> particles;
 std::vector<float> particlesPos;
 
+int frameCount = 0;
+float timeCount = 0.0f;
+
 
 GLShaderManager* shaderManager;
 
@@ -87,6 +90,13 @@ void updateParticles() {
     auto currentTime = std::chrono::steady_clock::now();
     float deltaTime = std::chrono::duration<float>(currentTime - shaderManager->startTime).count();
     shaderManager->startTime = currentTime;
+
+    timeCount += deltaTime;
+    if (timeCount >= 1.0f) {
+        LOGI("Frame rate: %d", frameCount);
+        frameCount = 0;
+        timeCount = 0.0f;
+    }
 
     for (auto& particle : particles) {
         particle.rk4Step(deltaTime, velocityField, b);
@@ -181,6 +191,8 @@ extern "C" {
 
         shaderManager->loadVectorFieldData(allVertices[currentFrame]);
         shaderManager->drawVectorField(numVertices);
+
+        frameCount++;
 //        updateFrame();
     }
 
