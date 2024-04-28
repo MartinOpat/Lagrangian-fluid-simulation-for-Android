afaadb9d0bf8f26ce7e84c67c468081319584567
MartinOpat <martinopat.opat@gmail.com>
2024-04-28 09:48:32 +0200

    There is an ibuilt fps counter

--- simulation/app/src/main/cpp/native-lib.cpp
+++ simulation/app/src/main/cpp/native-lib.cpp
@@ -28,10 +28,6 @@ float b = 0.8f;  // Drag coefficient
 std::vector<Particle> particles;
 std::vector<float> particlesPos;
 
-int frameCount = 0;
-float timeCount = 0.0f;
-
-
 GLShaderManager* shaderManager;
 
 void updateParticlePosArr() {
@@ -90,13 +86,6 @@ void updateParticles() {
     auto currentTime = std::chrono::steady_clock::now();
     float deltaTime = std::chrono::duration<float>(currentTime - shaderManager->startTime).count();
     shaderManager->startTime = currentTime;
-
-    timeCount += deltaTime;
-    if (timeCount >= 1.0f) {
-        LOGI("Frame rate: %d", frameCount);
-        frameCount = 0;
-        timeCount = 0.0f;
-    }
 
     for (auto& particle : particles) {
         particle.rk4Step(deltaTime, velocityField, b);
@@ -191,9 +180,7 @@ extern "C" {
 
         shaderManager->loadVectorFieldData(allVertices[currentFrame]);
         shaderManager->drawVectorField(numVertices);
-
-        frameCount++;
-//        updateFrame();
+        //        updateFrame();
     }
 
     JNIEXPORT void JNICALL Java_com_example_lagrangianfluidsimulation_MainActivity_setupGraphics(JNIEnv* env, jobject obj, jobject assetManager) {
