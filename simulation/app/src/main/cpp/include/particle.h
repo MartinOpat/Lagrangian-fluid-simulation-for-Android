eb434ebb3a1a8d04c4f3926370cc6d3a857cb953
MartinOpat <martinopat.opat@gmail.com>
2024-06-02 22:52:45 +0200

    Refactored particle

--- simulation/app/src/main/cpp/include/particle.h
+++ simulation/app/src/main/cpp/include/particle.h
@@ -5,20 +5,27 @@
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "consts.h"
-
+#include "android_logging.h"
 #include <vector>
 
-// Class declaration
+/**
+ * @class Particle
+ * @brief This class represents a particle in a physics simulation.
+ */
 class Particle {
 public:
-    // Constructor
+    /**
+     * @brief Constructor for the Particle class.
+     *
+     * @param initialPosition The initial position of the particle.
+     * @param initialVelocity The initial velocity of the particle.
+     * @param initialAcceleration The initial acceleration of the particle.
+     */
     Particle(glm::vec3 initialPosition = glm::vec3(0.0f), glm::vec3 initialVelocity = glm::vec3(0.0f), glm::vec3 initialAcceleration = glm::vec3(0.0f));
 
     glm::vec3 position;
     glm::vec3 velocity;
     glm::vec3 acceleration;
-private:
-
 };
 
 #endif // PARTICLE_H
--- simulation/app/src/main/cpp/src/particle.cpp
+++ simulation/app/src/main/cpp/src/particle.cpp
@@ -1,5 +1,4 @@
 #include "include/particle.h"
-#include "include/android_logging.h"
 
 Particle::Particle(glm::vec3 initialPosition, glm::vec3 initialVelocity, glm::vec3 initialAcceleration)
     : position(initialPosition), velocity(initialVelocity), acceleration(initialAcceleration)  {}
