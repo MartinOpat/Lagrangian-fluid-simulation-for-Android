2c09d0c64f1667b06f9f35d4efd11a3b1d5f3ca7
MartinOpat <martinopat.opat@gmail.com>
2024-06-02 22:44:56 +0200

    Refactored mainview

--- simulation/app/src/main/cpp/include/mainview.h
+++ simulation/app/src/main/cpp/include/mainview.h
@@ -23,40 +23,132 @@
 #include "shaderManager.h"
 
 
-
+/**
+ * @class Mainview
+ * @brief This class provides the main view for the application.
+ */
 class Mainview {
 public:
+    /**
+     * @brief Constructor
+     *
+     * @param assetManager A pointer to the asset manager.
+     */
     Mainview(AAssetManager* assetManager);
+
+    /**
+     * @brief Destructor
+     */
     ~Mainview();
 
+    /**
+     * @brief Sets the frame drawing
+     */
     void setFrame();
+
+    /**
+     * @brief Initial graphics setup
+     */
     void setupGraphics();
 
+    /**
+     * @brief Loads new vector field data.
+
+     * @param vertices A reference to the flat vector of vertices (6 floats per vector).
+     */
     void loadVectorFieldData(std::vector<float>& vertices);
+
+    /**
+     * @brief Draws the vector field.
+     *
+     * @param size Number of vectors to draw.
+     */
     void drawVectorField(int size);
+
+    /**
+     * @brief Creates a buffer for the vector field.
+     *
+     * @param vertices A reference to the flat vector of vertices (6 floats per vector).
+     */
     void createVectorFieldBuffer(std::vector<float>& vertices);
 
+    /**
+     * @brief Creates a buffer for the particles.
+     *
+     * @param particlesPos A reference to the flat vector of particle positions (3 floats per particle).
+     */
     void createParticlesBuffer(std::vector<float>& particlesPos);
+
+    /**
+     * @brief Loads particle data.
+     *
+     * @param particlesPos A reference to the flat vector of particle positions.
+     */
     void loadParticlesData(std::vector<float>& particlesPos);
+
+    /**
+     * @brief Draws the particles.
+     *
+     * @param size Number of particles to draw.
+     */
     void drawParticles(int size);
 
+    /**
+     * @brief Creates a compute buffer.
+     *
+     * @param vector_field0 A reference to the previous vector field.
+     * @param vector_field1 A reference to the next vector field.
+     * @param vector_field2 A reference to the vector field to be loaded next.
+     */
     void createComputeBuffer(std::vector<float>& vector_field0, std::vector<float>& vector_field1, std::vector<float>& vector_field2);
+
+    /**
+     * @brief Loads constant uniforms.
+     *
+     * @param dt Simulation he time step.
+     * @param width The width simulation dimension.
+     * @param height The height simulation dimension.
+     * @param depth The depth simulation dimension.
+     */
     void loadConstUniforms(float dt, int width, int height, int depth);
+
+    /**
+     * @brief Loads a compute buffer (not used for rendering) with new data so that it can be
+     * efficiently swapped when required.
+     *
+     * @param vector_field New vector field vertices to load.
+     * @param globalFence A reference to the global fence.
+     */
     void preloadComputeBuffer(std::vector<float>& vector_field, std::atomic<GLsync>& globalFence);
+
+    /**
+     * @brief Swaps the compute buffers. prev <- next, next <- future.
+     * @note This function is O(1).
+     */
     void loadComputeBuffer();
+
+    /**
+     * @brief Dispatches the compute shader to update the particle positions.
+     */
     void dispatchComputeShader();
 
+    /**
+     * @brief Getter for the object defining the view transformations.
+     *
+     * @return A reference to the transforms object.
+     */
     Transforms& getTransforms() { return *transforms; }
 
-    std::chrono::steady_clock::time_point startTime;
-
 private:
-    // Shaders
-    ShaderManager *shaderManager;
-
-    // Uniforms
+    /**
+     * @brief Loads the uniforms.
+     */
     void loadUniforms();
 
+    ShaderManager *shaderManager;
+    Transforms *transforms;
+
+    // Uniforms
     GLint isPointLocationLines;
     GLint isPointLocationPoints;
     GLint pointSize;
@@ -71,16 +163,11 @@ private:
     // Buffers
     GLuint particleVBO;
     GLuint particleVAO;
-
     GLuint vectorFieldVBO;
     GLuint vectorFieldVAO;
-
     GLuint computeVectorField0SSBO;
     GLuint computeVectorField1SSBO;
     GLuint computeVectorField2SSBO;
-
-    // Transforms
-    Transforms *transforms;
 };
 
 #endif // GL_SHADER_MANAGER_H
--- simulation/app/src/main/cpp/src/mainview.cpp
+++ simulation/app/src/main/cpp/src/mainview.cpp
@@ -1,7 +1,11 @@
+//
+// Created by martin on 08-05-2024.
+//
+
+
 #include "include/mainview.h"
 
 Mainview::Mainview(AAssetManager* assetManager) {
-    startTime = std::chrono::steady_clock::now();
     transforms = new Transforms();
     shaderManager = new ShaderManager(assetManager);
 }
@@ -54,22 +58,28 @@ void Mainview::setupGraphics() {
 
 void Mainview::createParticlesBuffer(std::vector<float>& particlesPos) {
     glUseProgram(shaderManager->shaderPointsProgram);
+
+    // Create VBO
     glGenBuffers(1, &particleVBO);
     glBindBuffer(GL_ARRAY_BUFFER, particleVBO);
     glBufferData(GL_ARRAY_BUFFER, particlesPos.size() * sizeof(float), particlesPos.data(), GL_STREAM_DRAW);
 
+    // Create VAO
     glGenVertexArrays(1, &particleVAO);
     glBindVertexArray(particleVAO);
 
+    // Enable vertex attribute array
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
 
+    // Unbind VAO and VBO
     glBindVertexArray(0);  // Unbind VAO
     glBindBuffer(GL_ARRAY_BUFFER, 0);  // Unbind VBO
 }
 
 void Mainview::loadParticlesData(std::vector<float>& particlesPos) {
     glUseProgram(shaderManager->shaderPointsProgram);
+
     glBindBuffer(GL_ARRAY_BUFFER, particleVBO);
     glBufferData(GL_ARRAY_BUFFER, particlesPos.size() * sizeof(float), particlesPos.data(), GL_STREAM_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -78,72 +88,93 @@ void Mainview::loadParticlesData(std::vector<float>& particlesPos) {
 void Mainview::drawParticles(int size) {
     glUseProgram(shaderManager->shaderPointsProgram);  // TODO: Possibly temporary
 
+    // Load VAO
     glBindVertexArray(particleVAO);
+
+    // Load uniforms
     glUniform1i(isPointLocationPoints, 1);
     glUniform1f(pointSize, 15.0f);
     glUniformMatrix4fv(modelLocationPoints, 1, GL_TRUE, &(transforms->modelTransform)[0][0]);
     glUniformMatrix4fv(projectionLocationPoints, 1, GL_TRUE, &(transforms->projectionTransform)[0][0]);
     glUniformMatrix4fv(viewLocationPoints, 1, GL_TRUE, &(transforms->viewTransform)[0][0]);
 
+    // Draw
     glDrawArrays(GL_POINTS, 0, size / 3);
 
+    // Unbind
     glBindVertexArray(0);
 }
 
 void Mainview::createVectorFieldBuffer(std::vector<float>& vertices) {
     glUseProgram(shaderManager->shaderLinesProgram);
+
+    // Create VBO
     glGenBuffers(1, &vectorFieldVBO);
     glBindBuffer(GL_ARRAY_BUFFER, vectorFieldVBO);
     glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STREAM_DRAW);
 
+    // Create VAO
     glGenVertexArrays(1, &vectorFieldVAO);
     glBindVertexArray(vectorFieldVAO);
 
+    // Enable vertex attribute array
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 
-    glBindVertexArray(0);  // Unbind VAO
-    glBindBuffer(GL_ARRAY_BUFFER, 0);  // Unbind VBO
+    // Unbind VAO and VBO
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
 void Mainview::loadVectorFieldData(std::vector<float>& vertices) {
     glUseProgram(shaderManager->shaderLinesProgram);
+
     glBindBuffer(GL_ARRAY_BUFFER, vectorFieldVBO);
     glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STREAM_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
 void Mainview::drawVectorField(int size) {
+    // Load VAO
     glBindVertexArray(vectorFieldVAO);
+
+    // Load uniforms
     glUniform1i(isPointLocationLines, 0);
     glUniformMatrix4fv(modelLocationLines, 1, GL_TRUE, &(transforms->modelTransform)[0][0]);
     glUniformMatrix4fv(projectionLocationLines, 1, GL_TRUE, &transforms->projectionTransform[0][0]);
     glUniformMatrix4fv(viewLocationLines, 1, GL_TRUE, &transforms->viewTransform[0][0]);
 
+    // Draw
     glDrawArrays(GL_LINES, 0, size / 3);
 
+    // Unbind
     glBindVertexArray(0);
 }
 
 // Create Buffers
 void Mainview::createComputeBuffer(std::vector<float>& vector_field0, std::vector<float>& vector_field1, std::vector<float>& vector_field2) {
+    // Create SSBO for previous vector field
     glGenBuffers(1, &computeVectorField0SSBO);
     glBindBuffer(GL_SHADER_STORAGE_BUFFER, computeVectorField0SSBO);
     glBufferData(GL_SHADER_STORAGE_BUFFER, vector_field0.size() * sizeof(float), vector_field0.data(), GL_DYNAMIC_DRAW);
 
+    // Create SSBO for next vector field
     glGenBuffers(1, &computeVectorField1SSBO);
     glBindBuffer(GL_SHADER_STORAGE_BUFFER, computeVectorField1SSBO);
     glBufferData(GL_SHADER_STORAGE_BUFFER, vector_field1.size() * sizeof(float), vector_field1.data(), GL_DYNAMIC_DRAW);
 
+    // Create SSBO for vector field to be loaded next
     glGenBuffers(1, &computeVectorField2SSBO);
     glBindBuffer(GL_SHADER_STORAGE_BUFFER, computeVectorField2SSBO);
     glBufferData(GL_SHADER_STORAGE_BUFFER, vector_field2.size() * sizeof(float), vector_field2.data(), GL_DYNAMIC_DRAW);
 
+    // Unbind
     glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
 }
 
 void Mainview::loadConstUniforms(float dt, int width, int height, int depth) {
     glUseProgram(shaderManager->shaderComputeProgram);
+
     glUniform1i(glGetUniformLocation(shaderManager->shaderComputeProgram, "width"), width);
     glUniform1i(glGetUniformLocation(shaderManager->shaderComputeProgram, "height"), height);
     glUniform1i(glGetUniformLocation(shaderManager->shaderComputeProgram, "depth"), depth);
@@ -155,10 +186,12 @@ void Mainview::loadConstUniforms(float dt, int width, int height, int depth) {
 }
 
 void Mainview::preloadComputeBuffer(std::vector<float>& vector_field, std::atomic<GLsync>& globalFence) {
+    // Load the new vector field into the SSBO not used for graphing
     glBindBuffer(GL_SHADER_STORAGE_BUFFER, computeVectorField2SSBO);
     glBufferData(GL_SHADER_STORAGE_BUFFER, vector_field.size() * sizeof(float), vector_field.data(), GL_DYNAMIC_DRAW);
     glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
 
+    // Setup fence to make sure the data is loaded and sync between threads before use
     GLsync fence = glFenceSync(GL_SYNC_GPU_COMMANDS_COMPLETE, 0);
     glFlush();
     globalFence.store(fence, std::memory_order_release);
@@ -169,15 +202,20 @@ void Mainview::loadComputeBuffer() {
     std::swap(computeVectorField1SSBO, computeVectorField2SSBO);
 }
 
-// Dispatch Compute Shader
 void Mainview::dispatchComputeShader() {
     glUseProgram(shaderManager->shaderComputeProgram);
+
+    // Load uniforms
     glUniform1f(globalTimeInStepLocation, global_time_in_step);
 
+    // Bind SSBOs
     glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, particleVBO); // Bind VBO as SSBO
     glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, computeVectorField0SSBO);
     glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 2, computeVectorField1SSBO);
 
+    // Dispatch
     glDispatchCompute((NUM_PARTICLES+255) / 256, 1, 1);
-    glMemoryBarrier(GL_VERTEX_ATTRIB_ARRAY_BARRIER_BIT); // Ensure vertex shader sees the updates
+
+    // Ensure vertex shader sees the updates
+    glMemoryBarrier(GL_VERTEX_ATTRIB_ARRAY_BARRIER_BIT);
 }
