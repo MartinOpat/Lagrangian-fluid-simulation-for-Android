83608fa2218e807d459d2cc3ec0fbf7eb36f4e47
MartinOpat <martinopat.opat@gmail.com>
2024-06-02 23:31:38 +0200

    Refactored transformations

--- simulation/app/src/main/cpp/include/transforms.h
+++ simulation/app/src/main/cpp/include/transforms.h
@@ -10,25 +10,65 @@
 #include "consts.h"
 #include "algorithm"
 
+/**
+ * @class Transforms
+ * @brief This class handles transformations in the application.
+ */
 class Transforms {
 public:
+    /**
+     * @brief Constructor.
+     */
     Transforms();
 
+    /**
+     * @brief Sets a new rotation trasformation.
+     *
+     * @param rotateX The rotation around the x-axis.
+     * @param rotateY The rotation around the y-axis.
+     * @param rotateZ The rotation around the z-axis.
+     */
     void setRotation(float rotateX, float rotateY, float rotateZ);
+
+    /**
+     * @brief Sets a new scale transformation.
+     *
+     * @param scale The new scale.
+     */
     void setScale(float scale);
+
+    /**
+     * @brief Sets a new aspect ratio.
+     *
+     * @param aspectRatio The new aspect ratio of the transformations.
+     */
     void setAspectRatio(float aspectRatio);
 
+    /**
+     * @brief Getter for the rotation of the transformations.
+     *
+     * @return The rotation of the transformations as a glm::vec3.
+     */
     glm::vec3 getRotation() { return rotation; }
+
+    /**
+     * @brief Getter for the scale of the transformations.
+     *
+     * @return The scale of the transformations as a float.
+     */
     float getScale() { return scale; }
 
-
-
+    // Transformation matrices
     glm::mat4 modelTransform;
     glm::mat4 projectionTransform;
     glm::mat4 viewTransform;
 
 private:
+    /**
+     * @brief Utility function to apply the current transfromations to the model matrix.
+     */
     void updateTransformations();
+
 
     glm::vec3 rotation;
     float scale;
--- simulation/app/src/main/cpp/src/transforms.cpp
+++ simulation/app/src/main/cpp/src/transforms.cpp
@@ -41,17 +41,15 @@ void Transforms::updateTransformations() {
     modelTransform *= glm::rotate(glm::identity<glm::mat4>(), -rotation.y, glm::vec3(0.0f, 1.0f, 0.0f));
     modelTransform *= glm::rotate(glm::identity<glm::mat4>(), rotation.z, glm::vec3(0.0f, 0.0f, 1.0f));
 
-    // Update projection matrix
 
     float width = FIELD_WIDTH / 0.5f;
     float height = FIELD_HEIGHT;
-
     if (aspectRatio > 0.5f) {
         width *= aspectRatio;
     } else {
         width /= aspectRatio;
     }
 
-
+    // Update the projection matrix according to the new aspect ratio
     projectionTransform = glm::ortho(-width, width, -height, height, -NEAR_FAR, NEAR_FAR);
 }
