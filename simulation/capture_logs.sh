127505d1cc1bf349e0a0d4beb55c2a7badb9ce03
MartinOpat <martinopat.opat@gmail.com>
2024-06-06 11:35:24 +0200

    Added simple bash script that captures timer logs

--- simulation/capture_logs.sh
+++ simulation/capture_logs.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 
-# Define the log file location
-LOGFILE="logs.txt"
+BASE_LOGFILE="logs.txt"
+
+# Find the next available log file name
+counter=0
+LOGFILE="${BASE_LOGFILE%.txt}$counter.txt"
+while [[ -f $LOGFILE ]]; do
+    let counter+=1
+    LOGFILE="${BASE_LOGFILE%.txt}$counter.txt"
+done
+
+echo "Logging to $LOGFILE"
+
+# Function to handle script termination
+cleanup() {
+    echo "Caught interrupt signal. Cleaning up..."
+    sync
+    exit 0
+}
+trap cleanup SIGINT
 
 # Check if adb is connected
-adb devices | grep -q device
-if [ $? -eq 0 ]; then
-    echo "Device is connected. Starting to capture logs..."
-else
+if ! adb devices | grep -q device; then
     echo "No device connected. Please connect your device and try again."
     exit 1
 fi
+echo "Device is connected. Starting to capture logs..."
 
 # Clear the current logs
 adb logcat -c
 
-# Start capturing logs. Adjust the grep pattern to match how the fps is logged.
-adb logcat | grep "FPS" >> $LOGFILE
+# Start capturing logs
+adb logcat | grep -E 'Timer\s*: Elapsed time: [0-9]+\.[0-9]+' >> $LOGFILE &
+ADB_PID=$!
 
+# Wait indefinitely until the script is killed
+wait $ADB_PID
