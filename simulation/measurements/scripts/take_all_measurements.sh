f3ae60c40f86879b71529831fe063abe7b847052
Martin <martinopat.opat@gmail.com>
2024-06-15 15:07:40 +0200

    More long measurements

--- simulation/measurements/scripts/take_all_measurements.sh
+++ simulation/measurements/scripts/take_all_measurements.sh
@@ -7,7 +7,7 @@ declare -a modes=("sequential" "parallel" "computeShaders")
 #declare -a particleCounts=(1500 3500 7500 17000 37000 85000 190000 420000 950000 2200000 5000000)
 #declare -a particleCounts=(5000000 2200000 950000 420000 190000 85000 37000 17000 7500 3500 1500)
 #declare -a particleCounts=(420000)
-declare -a particleCounts=(10000 1000000)
+declare -a particleCounts=(250 250000)
 #declare -a particleCounts=(1 5 25 125 625)
 #declare -a particleCounts=(3500)
 
