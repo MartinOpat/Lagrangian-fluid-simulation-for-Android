cbc24c383c2d9bf59cf35c7d8e74fc9cc9b00b63
Martin <martinopat.opat@gmail.com>
2024-06-18 17:06:35 +0200

    Added loader measurements

--- simulation/app/src/main/cpp/include/consts.h
+++ simulation/app/src/main/cpp/include/consts.h
@@ -14,7 +14,7 @@
 #define NEAR_FAR 100.0f
 
 // Number of simulation time between time steps (two files interpolation) == 1 day
-#define TIME_STEP 50
+#define TIME_STEP 10
 
 // [0, TIME_STEP)
 extern float global_time_in_step;
